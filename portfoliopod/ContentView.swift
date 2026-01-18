750e8590fd5ffebbe3f732ec34d4220c41bbb17f
anuraaagg <s.anurag1998@gmail.com>
2026-01-18 22:34:13 +0530

    Initial Commit

---
+++ portfoliopod/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  portfoliopod
+//
+//  Created by Anurag Singh on 18/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ portfoliopod/portfoliopodApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfoliopodApp.swift
+//  portfoliopod
+//
+//  Created by Anurag Singh on 18/01/26.
+//
+
+import SwiftUI
+
+@main
+struct portfoliopodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
